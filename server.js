59c74908a12be2745aef43a13fa89c9710d58114
jeffyptw <jeffyzazaza.rz@gmail.com>
2025-05-07 12:59:42 +0700

    Update App.js and server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
-
 const app = express();
+
+app.use(cors());
+
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, {
+  cors: {
+    origin: 'https://pokdeng-online-1.onrender.com',
+    methods: ['GET', 'POST']
+  }
+});
 
 const SUITS = ['♠', '♥', '♦', '♣'];
 const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
