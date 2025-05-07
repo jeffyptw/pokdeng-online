476b1edba7bc474b4379ac4560586e4e85154184
jeffyptw <jeffyzazaza.rz@gmail.com>
2025-05-08 04:48:16 +0700

    Fix: update socket URL to production

--- client/src/App.js
+++ client/src/App.js
@@ -149,7 +149,7 @@ function App() {
       setMyCards([]);
       setHasStayed(false);
       setRoomLocked(false);
-      setShowStartAgain(false); // แสดงปุ่มอีกครั้งในรอบต่อไป
+      setGameStarted(false); // แสดงปุ่มอีกครั้งในรอบต่อไป
     });
     socket.on('summaryData', data => {
       setSummaryData(data);
